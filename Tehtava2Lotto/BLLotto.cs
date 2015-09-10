b3fceec0eefd30f4d9dbd39fb6e4613a865ee952
Jere Halme <maasto_@hotmail.com>
2015-09-10 11:26:25 +0300

    progress

---
+++ Tehtava2Lotto/BLLotto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tehtava2Lotto
+{
+    class Lotto
+    {
+        public int minNumber;
+        public int maxNumber;
+        public int lkmNumber;
+
+        public Lotto (string)
+    }
+}
